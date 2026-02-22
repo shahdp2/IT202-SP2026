279313ef289cdc8c9a0b622cca3279e912458b80
shahdp2 <deepshah0206@gmail.com>
2026-02-21 22:03:18 -0500

    problem 2 plan

--- public_html/M2/scenario3.php
+++ public_html/M2/scenario3.php
@@ -3,7 +3,7 @@
 // @ts-nocheck
 require_once "base.php";
 
-$ucid = "mt85"; // <-- set your ucid
+$ucid = "dns33"; // <-- set your ucid
 
 // Don't edit the arrays below, they are used to test your code
 $array1 = [42, -17, 89, -256, 1024, -4096, 50000, -123456];
--- public_html/M2/scenario4.php
+++ public_html/M2/scenario4.php
@@ -3,7 +3,7 @@
 // @ts-nocheck
 require_once "base.php";
 
-$ucid = "mt85"; // <-- set your ucid
+$ucid = "dns33"; // <-- set your ucid
 
 // Don't edit the arrays below, they are used to test your code
 $array1 = ["hello world!", "php programming", "special@#$%^&characters", "numbers 123 456", "mIxEd CaSe InPut!"];
