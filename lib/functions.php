78ef22c925af8a0a996ddd7967bde005442a5b73
shahdp2 <deepshah0206@gmail.com>
2026-03-25 12:28:21 -0400

    user registration: save data to database with password hashing

--- lib/functions.php
+++ lib/functions.php
@@ -1,5 +1,6 @@
 <?php
 //TODO 1: require db.php
+require(__DIR__ . "/db.php");
 
 //require safer_echo.php
 require(__DIR__ . "/safer_echo.php");
--- public_html/project/register.php
+++ public_html/project/register.php
@@ -67,7 +67,19 @@ require(__DIR__."/../../lib/functions.php");
     }
 
     if (!$hasError) {
-        echo "Success<br>";
+        // TODO 4: Hash password and store record in DB
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $db = getDB(); // available due to the `require()` of `functions.php`
+        // Code for inserting user data into the database
+        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)");
+        try{
+            $stmt->execute([':email' => $email, ':password' => $hashed_password]);
+            echo "Successfully registered!<br>";
+        }
+        catch(Exception $e){
+            echo "There was an error registering<br>"; // user-friendly message
+            error_log("Registration Error: " . var_export($e, true)); // log the technical error for debugging
+        }
     }
 }
 ?>
