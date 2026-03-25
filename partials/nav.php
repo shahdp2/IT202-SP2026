4d50d2be7b677675e4f04bc68cdfb74e31b68811
shahdp2 <deepshah0206@gmail.com>
2026-03-25 12:46:14 -0400

    basic nav and login part 1

---
+++ partials/nav.php
@@ -0,0 +1,10 @@
+<?php
+require(__DIR__."/../lib/functions.php"); 
+?>
+<nav> 
+    <ul>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
