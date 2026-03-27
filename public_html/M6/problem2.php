ff60cf5f833a3d0c2eb267c912956515af8c06b1
shahdp2 <deepshah0206@gmail.com>
2026-03-26 20:57:13 -0400

    Problem 2 solution comments

--- public_html/M6/problem2.php
+++ public_html/M6/problem2.php
@@ -30,6 +30,11 @@ function processCars($cars) {
     $processedCars = []; // result array
     $classic_age = 25; // don't change this value
     // Start edits
+    // UCID: dns33
+    // Date: 2026-03-27
+    // Plan: 1) get current year, 2) loop cars, 3) compute age, 4) set isClassic based on $classic_age, 5) push into $processedCars
+
+
    
     // End edits
     echo "<pre>" . var_export($processedCars, true) . "</pre>";
