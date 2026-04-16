c486feed3571450df053e875d3595914eb029423
shahdp2 <deepshah0206@gmail.com>
2026-04-16 18:25:48 -0400

    solution

--- partials/nav.php
+++ partials/nav.php
@@ -3,45 +3,51 @@
 //that way we don't need to include so many other files on each page
 //nav will pull in functions and functions will pull in db
 
+
 // checking to see if domain has a port number attached (localhost)
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', '0');
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
-    // strip the port number if present
-    $domain = explode(":", $domain)[0];
+   // strip the port number if present
+   $domain = explode(":", $domain)[0];
 }
 // used for public hosting like heroku
 if ($domain != "localhost") {
-    session_set_cookie_params([
-        "lifetime" => 60 * 60, // this is cookie lifetime, not session lifetime
-        "path" => $BASE_PATH, // path to restrict cookie to; match your project folder (case sensitive)
-        "domain" => $domain, // domain to restrict cookie to
-        "secure" => true, // https only
-        "httponly" => true, // javascript can't access
-        "samesite" => "lax" // helps prevent CSRF, but allows normal navigation
-    ]);
+   session_set_cookie_params([
+       "lifetime" => 60 * 60, // this is cookie lifetime, not session lifetime
+       "path" => "/project", // path to restrict cookie to; match your project folder (case sensitive)
+       "domain" => $domain, // domain to restrict cookie to
+       "secure" => true, // https only
+       "httponly" => true, // javascript can't access
+       "samesite" => "lax" // helps prevent CSRF, but allows normal navigation
+   ]);
 }
 session_start();
 require(__DIR__."/../lib/functions.php");
 ?>
+
+
 <link rel="stylesheet" href="<?php get_url('styles.css', true);?>">
 <script src="<?php get_url('helpers.js', true);?>"></script>
+<script src="https://matttoegel.github.io/IT202-Utils/submission-utils.js"></script>
 <nav>
-    <ul>
-        <?php if (is_logged_in()) : ?>
-            <li><a href="<?php get_url('landing.php', true);?>">Landing</a></li>
-            <li><a href="<?php get_url('profile.php', true);?>">Profile</a></li>
-        <?php endif; ?>
-        <?php if (!is_logged_in()) : ?>
-            <li><a href="<?php get_url('login.php', true);?>">Login</a></li>
-            <li><a href="<?php get_url('register.php', true);?>">Register</a></li>
-        <?php endif; ?>
-        <?php if (has_role("Admin")) : ?>
-            <li><a href="<?php get_url('admin/create_role.php', true); ?>">Create Role</a></li>
-            <li><a href="<?php get_url('admin/list_roles.php', true); ?>">List Roles</a></li>
-            <li><a href="<?php get_url('admin/assign_roles.php', true); ?>">Assign Roles</a></li>
-        <?php endif; ?>
-        <?php if (is_logged_in()) : ?>
-            <li><a href="<?php get_url('logout.php', true);?>">Logout</a></li>
-        <?php endif; ?>
-    </ul>
+   <ul>
+       <?php if (is_logged_in()) : ?>
+           <li><a href="<?php get_url('landing.php', true);?>">Landing</a></li>
+           <li><a href="<?php get_url('profile.php', true);?>">Profile</a></li>
+       <?php endif; ?>
+       <?php if (!is_logged_in()) : ?>
+           <li><a href="<?php get_url('login.php', true);?>">Login</a></li>
+           <li><a href="<?php get_url('register.php', true);?>">Register</a></li>
+       <?php endif; ?>
+       <?php if (has_role("Admin")) : ?>
+           <li><a href="<?php get_url('admin/create_role.php', true); ?>">Create Role</a></li>
+           <li><a href="<?php get_url('admin/list_roles.php', true); ?>">List Roles</a></li>
+           <li><a href="<?php get_url('admin/assign_roles.php', true); ?>">Assign Roles</a></li>
+       <?php endif; ?>
+       <?php if (is_logged_in()) : ?>
+           <li><a href="<?php get_url('logout.php', true);?>">Logout</a></li>
+       <?php endif; ?>
+   </ul>
 </nav>
