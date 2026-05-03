2863114d7d1c5f1f0218d848bda4d5990e6e2bd6
shahdp2 <deepshah0206@gmail.com>
2026-05-03 15:54:13 -0400

    first

--- partials/nav.php
+++ partials/nav.php
@@ -62,6 +62,7 @@ $hide_main_links = ($current === "landing.php");
                     <a href="<?php get_url('admin/create_role.php', true); ?>">Create Role</a>
                     <a href="<?php get_url('admin/list_roles.php', true); ?>">List Roles</a>
                     <a href="<?php get_url('admin/assign_roles.php', true); ?>">Assign Roles</a>
+                    <a href="<?php get_url('admin/list_user_video_associations.php', true); ?>">Video Associations</a>
                 </div>
             </li>
 
